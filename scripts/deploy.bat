5133035a3918856af18d504174eacff287cb1f24
Wei Li <wei.li@mclaren.com>
2021-08-10 12:27:47 +0100

    Skip deploying if not running as admin

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,7 +1,12 @@
-if exist "C:\Program Files\McLaren Applied Technologies\ATLAS 10\" (
-  xcopy /r /y %1 "C:\Program Files\McLaren Applied Technologies\ATLAS 10"
-)else (
-  echo "No Atlas installation detected - skipping deploying"
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    IF EXIST "C:\Program Files\McLaren Applied Technologies\ATLAS 10\" (
+      xcopy /r /y %1 "C:\Program Files\McLaren Applied Technologies\ATLAS 10"
+    )ELSE (
+      echo "No Atlas installation detected - skip deploying"
+    )
+    rem xcopy /r /y %1 "C:\dev\MAT.OCS.Atlas10\bin\Debug"
+) ELSE (
+    Echo "This script requires admin privillages - skip deploying"
+    Exit /b 0
 )
-
-rem xcopy /r /y %1 "C:\dev\MAT.OCS.Atlas10\bin\Debug"
