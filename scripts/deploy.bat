6bd8d5d338768590002c4e0de48959d3fae9c999
Wei Li <wei.li@mclaren.com>
2021-08-10 12:08:51 +0100

    Make sure deploy.bat works on all machines

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,3 +1,7 @@
-xcopy /r /y %1 "C:\Program Files\McLaren Applied Technologies\ATLAS 10"
+if exist "C:\Program Files\McLaren Applied Technologies\ATLAS 10\" (
+  xcopy /r /y %1 "C:\Program Files\McLaren Applied Technologies\ATLAS 10"
+)else (
+  echo "No Atlas installation detected - skipping deploying"
+)
 
 rem xcopy /r /y %1 "C:\dev\MAT.OCS.Atlas10\bin\Debug"
