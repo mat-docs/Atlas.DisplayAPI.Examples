2b97d0d6d6a1fc665de1042e6a52b8e03e7c810e
Mykola Kolodnytskyy <mykola.kolodnytskyy@mclaren.com>
2023-12-14 17:38:25 +0000

    83957 - Display API Example does not work

---
+++ MAT.Atlas.Plugins.Samples.HelloWorld/Resources/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MAT.Atlas.Plugins.Samples.HelloWorld")]
+[assembly: AssemblyDescription("Samples Hello HelloWorld")]
+[assembly: Guid("54DDB988-7E5B-4696-A0D4-121E653C6BB0")]
+
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows")]
